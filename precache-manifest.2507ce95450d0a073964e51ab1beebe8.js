636aff6c72f419d674a40dfc17208428bf190ab2
aaqib925 <muhammadaaqib925@gmail.com>
2021-02-03 22:16:15 +0500

    Updates

---
+++ precache-manifest.2507ce95450d0a073964e51ab1beebe8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3616d57efac4a3dd953ca0b6af5f3895",
+    "url": "/Dashboard-App/index.html"
+  },
+  {
+    "revision": "a46a55e833fe15f071b9",
+    "url": "/Dashboard-App/static/css/2.83fe24e5.chunk.css"
+  },
+  {
+    "revision": "5db1024dc9ca7a47571f",
+    "url": "/Dashboard-App/static/css/main.a5babc6c.chunk.css"
+  },
+  {
+    "revision": "a46a55e833fe15f071b9",
+    "url": "/Dashboard-App/static/js/2.edfb47f8.chunk.js"
+  },
+  {
+    "revision": "537154512aea58f04eecd54fe95c2f21",
+    "url": "/Dashboard-App/static/js/2.edfb47f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "336c46aeb3704517d3b0",
+    "url": "/Dashboard-App/static/js/3.4f7d18d4.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/Dashboard-App/static/js/3.4f7d18d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7281a29ad2d895bb1ac4",
+    "url": "/Dashboard-App/static/js/4.ae2304bd.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/Dashboard-App/static/js/4.ae2304bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e291db56bd55b5e062",
+    "url": "/Dashboard-App/static/js/5.5dbe2c62.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/Dashboard-App/static/js/5.5dbe2c62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5db1024dc9ca7a47571f",
+    "url": "/Dashboard-App/static/js/main.53d3ee4f.chunk.js"
+  },
+  {
+    "revision": "fb6192f30588b9b9c12b",
+    "url": "/Dashboard-App/static/js/runtime-main.984de28d.js"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/Dashboard-App/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/Dashboard-App/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/Dashboard-App/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/Dashboard-App/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Dashboard-App/precache-manifest.07b7532e82e1fe12bc77b25a816d83ec.js"
+  "/Dashboard-App/precache-manifest.2507ce95450d0a073964e51ab1beebe8.js"
 );
 
 self.addEventListener('message', (event) => {
